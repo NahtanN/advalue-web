abe53398887895ee42c1d779f7f44b40ca7840d4
Nathan Gomes <nahtann@outlook.com>
2021-02-25 18:58:45 -0300

    products template created

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,125 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import { FiHeart, FiShoppingCart, FiXCircle } from 'react-icons/fi'
 
 import Header from '../components/Header'
 
+import styles from '../styles/pages/index.module.css'
+
 export default function Home() {
   return (
-      <div className="container">
+      <div>
         <Head>
             <title>Vital - Home</title>
         </Head>
 
         <Header />
+
+        <div className={styles.wrapper}>
+          <section className={styles.container}>
+            <div className={styles.product}>
+              <div className={styles.image_container}>
+                  <Image
+                    id="te" 
+                    priority
+                    src="/images/bitcoin.jpeg"                          
+                    layout="fill"
+                    objectFit="fill"
+                    quality={100}
+                    alt="bitcoin"
+                    className={styles.image}
+                  />
+              </div>
+
+              <h1 className={styles.title}>Vitae tortor condimentum lacinia quis vel</h1>
+              <span className={styles.category}>Categoria</span>
+              <p className={styles.price}>$ 300</p>
+
+              <div className={styles.interactions}>
+                <FiHeart id={styles.heart}/>
+                <FiShoppingCart id={styles.cart}/>
+              </div>
+            </div>
+          </section>
+
+          <section className={styles.container}>
+            <div className={styles.product}>
+              <div className={styles.image_container}>
+                  <Image
+                    id="te" 
+                    priority
+                    src="/images/bitcoin.jpeg"                          
+                    layout="fill"
+                    objectFit="fill"
+                    quality={100}
+                    alt="bitcoin"
+                    className={styles.image}
+                  />
+              </div>
+
+              <h1 className={styles.title}>Vitae tortor condimentum lacinia quis vel</h1>
+              <span className={styles.category}>Categoria</span>
+              <p className={styles.price}>$ 300</p>
+
+              <div className={styles.interactions}>
+                <FiHeart id={styles.heart}/>
+                <FiShoppingCart id={styles.cart}/>
+              </div>
+            </div>
+          </section>
+
+          <section className={styles.container}>
+            <div className={styles.product}>
+              <div className={styles.image_container}>
+                  <Image
+                    id="te" 
+                    priority
+                    src="/images/bitcoin.jpeg"                          
+                    layout="fill"
+                    objectFit="fill"
+                    quality={100}
+                    alt="bitcoin"
+                    className={styles.image}
+                  />
+              </div>
+
+              <h1 className={styles.title}>Vitae tortor condimentum lacinia quis vel</h1>
+              <span className={styles.category}>Categoria</span>
+              <p className={styles.price}>$ 300</p>
+
+              <div className={styles.interactions}>
+                <FiHeart id={styles.heart}/>
+                <FiShoppingCart id={styles.cart}/>
+              </div>
+            </div>
+          </section>
+          
+          <section className={styles.container}>
+            <div className={styles.product}>
+              <div className={styles.image_container}>
+                  <Image
+                    id="te" 
+                    priority
+                    src="/images/bitcoin.jpeg"                          
+                    layout="fill"
+                    objectFit="fill"
+                    quality={100}
+                    alt="bitcoin"
+                    className={styles.image}
+                  />
+              </div>
+
+              <h1 className={styles.title}>Vitae tortor condimentum lacinia quis vel</h1>
+              <span className={styles.category}>Categoria</span>
+              <p className={styles.price}>$ 300</p>
+
+              <div className={styles.interactions}>
+                <FiHeart id={styles.heart}/>
+                <FiXCircle id={styles.cart}/>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
   )
 }
