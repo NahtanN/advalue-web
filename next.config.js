fd10fdb35db49bbc04b802820d7779edf28fbd06
Nathan Gomes <nahtann@outlook.com>
2021-04-09 15:02:44 -0300

    next cofing added

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    images: {
+        domains: [
+            process.env.DOMAIN_IMAGE_AMZ_1,
+            process.env.DOMAIN_IMAGE_AMZ_2,
+        ],
+    },
+    env: {
+        API_URL: process.env.API_URL
+    }
+}
